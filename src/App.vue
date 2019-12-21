952a79b49f8054497d5ff850fbbdeb1a01f2b43a
Valeriya Orekhova <orekhova.lera@gmai.com>
2019-12-21 18:29:23 +0300

    Succesfully finished

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Container>
+      <ChatWindow @send-message="sendMessages">
+        <ChatMessage v-for="message in messages" :key="message.id" :username="message.author" :datetime="message.datetime">{{message.text}} </ChatMessage>
+      </ChatWindow>
+    </Container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import ChatMessage from './components/ChatMessage.vue'
+import ChatWindow from './components/ChatWindow.vue'
+import Container from './components/Container.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
-}
+    ChatWindow,ChatMessage,Container
+  },
+  data(){
+    return {messages:[]}
+},
+  methods:{
+    getMessages(){
+      axios.get('http://188.225.47.187/api/chat/getmessages.php').then((response)=>{
+        this.messages=response.data.sort((a,b) => a.id - b.id)
+    })
+    },
+    sendMessages(obj){
+      axios.post('http://188.225.47.187/api/chat/sendmessage.php',{author: obj.nickname,
+        text: obj.message}).then(()=>{
+        this.getMessages()
+      })
+    }
+
+  },
+    mounted(){
+      setInterval(()=>{
+        this.getMessages()},3000
+      )
+
+  },
+  }  
+  
+
 </script>
 
 <style>
@@ -24,5 +55,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  margin: 0;
+  background-color: #f9f9fa;
 }
 </style>
---
+++ src/components/Container.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container">
+        <slot></slot>    
+    </div>
+</template>
+<style >
+.container {
+  width: 764px;
+  margin: auto;
+  box-shadow: 0 0 15px 0 #5f5f5f73;
+}
+</style>
